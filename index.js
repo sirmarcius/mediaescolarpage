b372912fb2675b16524ed2afb09ebd5d5b4d72fe
Márcio Gusmão <marciogusmao@msn.com>
2022-04-14 03:15:31 -0300

    Atualização de código

--- index.js
+++ index.js
@@ -1,36 +1,24 @@
 
 function calcular() {
-
-
-let n1 = parseFloat(document.getElementById('nt1').value)
-
-let n2 = parseFloat(document.getElementById('nt2').value)
-
-let n3 = parseFloat(document.getElementById('nt3').value)
-
-let n4 = parseFloat(document.getElementById('nt4').value)
-
-let resultado = (n1 + n2 + n3 + n4) / 4
   
-document.querySelector('#resultado').innerHTML = ('A média é: ') + resultado
+  let n1 = parseFloat(document.getElementById("nt1").value);
+  let n2 = parseFloat(document.getElementById("nt2").value);
+  let n3 = parseFloat(document.getElementById("nt3").value);
+  let n4 = parseFloat(document.getElementById("nt4").value);
+  
+  let resultado = (n1+n2+n3+n4)/4;
+  
+  document.querySelector(".result").innerHTML = "\n\nA média é: " + resultado.toFixed(2);
 
 }
 
 function limpar() {
-     document.getElementById('nt1').value = null
-document.getElementById('nt2').value = null
-document.getElementById('nt3').value = null
-document.getElementById('nt4').value = null  
+  
+  document.getElementById("nt1").value = null;
+  document.getElementById("nt2").value = null;
+  document.getElementById("nt3").value = null;
+  document.getElementById("nt4").value = null; 
   
 }
-
-
-
-
-
-
-
-
-
 
 
