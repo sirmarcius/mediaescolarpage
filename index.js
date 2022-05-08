80f49a17825853c8605262ac763b14c58c337570
Márcio Gusmão <marciogusmao@msn.com>
2022-05-08 15:23:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
-// Função que calcula a média escolar.
 function calcular() {
-  
   let n1 = parseFloat(document.getElementById("nt1").value);
   let n2 = parseFloat(document.getElementById("nt2").value);
   let n3 = parseFloat(document.getElementById("nt3").value);
   let n4 = parseFloat(document.getElementById("nt4").value);
-  
-  let resultado = (n1+n2+n3+n4)/4;
-    
-  document.querySelector(".result").innerHTML = "\n\nA média é: " + resultado.toFixed(1);
 
+  let resultado = (n1 + n2 + n3 + n4) / 4;
+
+  document.querySelector(".result").innerHTML =
+    "\n\nA média é: " + resultado.toFixed(1);
 }
 
-// Função responsavel por limpar os dados dos inputs da tela.
 function limpar() {
-  
   document.getElementById("nt1").value = null;
   document.getElementById("nt2").value = null;
   document.getElementById("nt3").value = null;
-  document.getElementById("nt4").value = null; 
-  
+  document.getElementById("nt4").value = null;
+
+  let resultado = "";
+  document.querySelector(".result").innerHTML = "\n\n" + resultado;
 }
-
-
