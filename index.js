9c7b3aac61bb65cae04ca5a5f26d2865a60a4db8
Márcio Gusmão <marciogusmao@msn.com>
2022-09-05 21:00:32 -0300

    Update index.js
    Alteração no código para melhorias

--- index.js
+++ index.js
@@ -4,18 +4,19 @@ function calcular() {
   let n3 = parseFloat(document.getElementById("nt3").value);
   let n4 = parseFloat(document.getElementById("nt4").value);
 
-  let resultado = (n1 + n2 + n3 + n4) / 4;
-
-  document.querySelector(".result").innerHTML =
+  //Converte o que foi pego do formulario pra number
+  let resultado = Number((n1 + n2 + n3 + n4) / 4);
+  //valida o conteudo que foi pegado no input
+  if (resultado.toString() === "NaN") return;
+  //sai da função caso o calculo de errado
+  document.querySelector(".result").innerText =
     "\n\nA média é: " + resultado.toFixed(1);
 }
-
 function limpar() {
   document.getElementById("nt1").value = null;
   document.getElementById("nt2").value = null;
   document.getElementById("nt3").value = null;
   document.getElementById("nt4").value = null;
-
   let resultado = "";
-  document.querySelector(".result").innerHTML = "\n\n" + resultado;
+  document.querySelector(".result").innerText = "\n\n" + resultado;
 }
